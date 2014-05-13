b79cb9aa218ceab7213964a76dca0b3ddc2af850
Arielle B Cruz <arielle.cruz@gmail.com>
2014-05-13 12:12:02 +0800

    Updated settings.php, added a comment.

--- settings.php
+++ settings.php
@@ -2,7 +2,10 @@
 
 // Because we don't like hardcoding stuff like this in our scripts.
 
+// SITE_DOMAIN value must be http:/<domain-name-or-ip>[:port][/path-to-here]/
+// Must be changed per instance.
 define("SITE_DOMAIN", "http://0.0.0.0:5000/");
+
 // For exploding in index.php and encoding in field.js.
 define("VALUE_DELIMITER", ":;:");
 
